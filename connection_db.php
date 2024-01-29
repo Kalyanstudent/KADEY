d22fb1e5fe3237819267a949b69ef31cc938fce8
Kalyanstudent <141897830+Kalyanstudent@users.noreply.github.com>
2024-01-29 03:33:57 -0600

    Update connection_db.php

--- connection_db.php
+++ connection_db.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(0);
 $servername ="localhost";
 $username   ="root";
 $password   ="";
@@ -11,7 +11,7 @@ $conn =mysqli_connect($servername,$username,$password,$dbname);
 if($conn){
     echo "connection ok";
 }else{
-    echo "connection failed"/*.mysqli_connect_error()*/;
+    echo "connection failed".mysqli_connect_error();
 }
 
 ?>
